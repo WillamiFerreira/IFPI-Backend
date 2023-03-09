bd45a8f7b82f7392b3f223c6720b2e9a428f6715
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-03-09 20:34:05 -0300

    Modularização feita

---
+++ Atividades/TarefaAPI/modules/utils.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException, status
+
+def formatar_valores(tarefa):
+    tarefa.descricao = tarefa.descricao.capitalize()
+    tarefa.responsavel = tarefa.responsavel.capitalize()
+    tarefa.situacao = tarefa.situacao.capitalize()
+    
+    return tarefa
+
+def not_found_error_message(tarefa_id):
+    """Função que retorna um mensagem de erro 404"""
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'Tarefa com ID {tarefa_id} não encontrada.')
+    
--- Atividades/TarefaAPI/tarefasAPI.py
+++ Atividades/TarefaAPI/tarefasAPI.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 
+from modules.utils import formatar_valores, not_found_error_message
+
 app = FastAPI()
 
 class Tarefa(BaseModel):
@@ -13,19 +15,6 @@ class Tarefa(BaseModel):
 
 tarefas: list[Tarefa] = []
 
-
-def formatar_valores(tarefa):
-    tarefa.descricao = tarefa.descricao.capitalize()
-    tarefa.responsavel = tarefa.responsavel.capitalize()
-    tarefa.situacao = tarefa.situacao.capitalize()
-    
-    return tarefa
-
-def not_found_error_message(tarefa_id):
-    """Função que retorna um mensagem de erro 404"""
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'Tarefa com ID {tarefa_id} não encontrada.')
-    
 
 @app.post("/tarefa", status_code=status.HTTP_201_CREATED)
 def adicionar_tarefa(tarefa: Tarefa):
