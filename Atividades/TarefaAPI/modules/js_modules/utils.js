4eb340da2351c86e131565775aee400a639d8a52
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-03-11 19:56:37 -0300

    update

--- Atividades/TarefaAPI/modules/js_modules/utils.js
+++ Atividades/TarefaAPI/modules/js_modules/utils.js
@@ -1,5 +1,27 @@
 function obterValorDoRadioButtons(name) {
     return document.querySelector(`input[name=${name}]:checked`).value
 }
+function buildButtons(){
+    const buttons_cont = document.createElement('div');
 
-export default obterValorDoRadioButtons;
+    const edit_btn = document.createElement('a');
+    edit_btn.innerHTML = 'Edit';
+    edit_btn.href = "#"
+
+    const delete_btn = document.createElement('a');
+    delete_btn.innerHTML = 'Delete';
+    delete_btn.href = "#"
+
+    buttons_cont.appendChild(edit_btn);
+    buttons_cont.appendChild(delete_btn);
+
+    return buttons_cont;
+
+}
+
+function resetarForm(){
+    const form_task = document.getElementById('form-task') /*captura o formulário no html */
+    form_task.reset() /*reseta todos os valores do input */
+}
+
+export {obterValorDoRadioButtons, buildButtons, resetarForm};
--- Atividades/TarefaAPI/public/script.js
+++ Atividades/TarefaAPI/public/script.js
@@ -1,41 +1,135 @@
 const baseURL = 'http://127.0.0.1:8000/tarefa'
-import obterValorDoRadioButtons from "../modules/js_modules/utils.js";
+import {
+    obterValorDoRadioButtons,
+    buildButtons,
+    resetarForm
+} from "../modules/js_modules/utils.js";
 
 let tasks = [];
 
-function atualizar_tela(){
-    const task_cont = document.querySelector('.task-cont');
+function filtrarTarefas() {
+    const level_filter = document.querySelector('#level-filter');
+    const priority_filter = document.querySelector('#priority-filter');
+
+    const filtrar_btn = document.querySelector('#filtrar-btn');
+
+    filtrar_btn.onclick = () => {
+        //obtendo o valor de level filtrado pelo user
+        var selectedIndexL = level_filter.selectedIndex;
+        var selectedOptionL = level_filter.options[selectedIndexL];
+        var selectedTextL = selectedOptionL.value;
+
+        //obtendo o valor de priority filtrado pelo user
+        var selectedIndexP = priority_filter.selectedIndex;
+        var selectedOptionP = priority_filter.options[selectedIndexP];
+        var selectedTextP = selectedOptionP.value;
+
+        selectedTextP = Number(selectedTextP)
+        selectedTextL = Number(selectedTextL)
+
+
+
+        console.log(typeof(selectedTextP));
+
+
+        atualizar_tela(selectedTextL, selectedTextP)
+    }
+
+
+}
+
+function atualizar_tela(l = null, p = null) {
+
+    var task_cont = document.querySelector('.task-cont');
     task_cont.innerHTML = []
 
-    for (let task of tasks){
-        const item = document.createElement('div')
-        item.classList.add('task-item')
+    for (let task of tasks) {
+        if (l == 'name' || null && p == 'name' || null) { //Entre se o user não tiver seleciona um filtro
 
-        var task_info = []
+            const item = document.createElement('div')
+            item.classList.add('task-item')
 
-        for (let i in task) {
-            item.innerText = item.innerHTML + task[i]
+            for (var i = 0; i < 7; ++i) {
+                var item_cell = document.createElement('div')
+                item.appendChild(item_cell)
+                item_cell.classList.add('task-cell')
+
+                if (i == 0) {
+                    var checkbox_bottom = document.createElement('input')
+                    checkbox_bottom.type = 'checkbox'
+                    item_cell.appendChild(checkbox_bottom)
+
+                } else if (i == 1) {
+                    item_cell.innerText = task.descricao;
+                } else if (i == 2) {
+                    item_cell.innerText = task.prioridade
+                } else if (i == 3) {
+                    item_cell.innerText = task.nivel
+                } else if (i == 4) {
+                    item_cell.innerText = task.situacao
+                } else if (i == 5) {
+                    item_cell.innerText = task.responsavel
+                } else {
+                    item_cell.appendChild(buildButtons())
+                }
+            }
+
+            task_cont.appendChild(item)
+
+
+        } else { // se o user seleciona algum filtro
+            if (task.nivel == l || task.prioridade == p) {
+
+                const item = document.createElement('div')
+                item.classList.add('task-item')
+
+                for (var i = 0; i < 7; ++i) {
+                    var item_cell = document.createElement('div')
+                    item.appendChild(item_cell)
+                    item_cell.classList.add('task-cell')
+
+                    if (i == 0) {
+                        var checkbox_bottom = document.createElement('input')
+                        checkbox_bottom.type = 'checkbox'
+                        item_cell.appendChild(checkbox_bottom)
+
+                    } else if (i == 1) {
+                        item_cell.innerText = task.descricao;
+                    } else if (i == 2) {
+                        item_cell.innerText = task.prioridade
+                    } else if (i == 3) {
+                        item_cell.innerText = task.nivel
+                    } else if (i == 4) {
+                        item_cell.innerText = task.situacao
+                    } else if (i == 5) {
+                        item_cell.innerText = task.responsavel
+                    } else {
+                        item_cell.appendChild(buildButtons())
+                    }
+                }
+
+                task_cont.appendChild(item)
+            } else {
+                console.log("Erro aqui")
+            }
         }
 
-
-        task_cont.appendChild(item)
     }
 }
 
-async function carregar_tarefas(){
+async function carregar_tarefas() {
     const response = await fetch(baseURL)
 
     const status = response.status
     tasks = await response.json()
 
-    //atualizar_tela()
     atualizar_tela()
 }
 
-function configurar_formulario(){
+function configurar_formulario() {
     const form_task = document.querySelector('#form-task');
 
-    form_task.onsubmit = async function(event){
+    form_task.onsubmit = async function (event) {
         event.preventDefault();
 
         const dados = form_task.children
@@ -44,7 +138,13 @@ function configurar_formulario(){
         const prioridade = obterValorDoRadioButtons("priori-option")
         const nivel = obterValorDoRadioButtons("level-option")
 
-        const task = {descricao, responsavel, nivel, prioridade}
+        const task = {
+            descricao,
+            responsavel,
+            nivel,
+            prioridade
+        }
+
 
         const opcoes = {
             method: "POST",
@@ -55,18 +155,22 @@ function configurar_formulario(){
         }
         const response = await fetch(baseURL, opcoes)
 
-        if(response.status === 201){
+        if (response.status === 201) {
             alert('Tarefa cadastrada com sucesso!')
             carregar_tarefas()
+            resetarForm()
 
-        } else{
+        } else {
             alert('Terefa não cadastrada')
         }
     }
 }
 
-function app(){
+function app() {
     configurar_formulario()
+    carregar_tarefas()
+    filtrarTarefas()
 }
+
 
 app()
